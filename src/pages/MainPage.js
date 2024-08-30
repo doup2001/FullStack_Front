945e416ad35e2f46d9be4664c94a92731a702e2d
eedo_y <doup2001@naver.com>
2024-08-30 15:06:46 +0900

    feat : [SPA] Link활용한 이동 방식

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function MainPage(props) {
-    return (
-        <div className={'text-3xl'}>
-            <div>
-                MainPage
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className={'text-3xl'}>MainPage</div>
+      <div>
+        <Link to='/about'>About</Link>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
